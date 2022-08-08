327e35fef1d14819420c585af4e78363ae694e31
Lody <69472620+bilhox@users.noreply.github.com>
2022-08-08 16:20:06 +0200

    V0.7

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from scripts.camera import *
 from scripts.entity import *
 from scripts.map import TileMap
 from scripts.text import *
+from scripts.button import *
 
 
 def main():
@@ -54,141 +55,200 @@ def main():
     
     level_ended = False
     
-    font = Font("./data/fonts/large_font.png" , [255]*3)
-    finished_text = Text(font , "Congrulations ! You found the tea recipe !")
-    finished_text.origin = finished_text.size/2
-    finished_text.pos = pygame.Vector2(200 , 100)
+    lfont = Font("./data/fonts/large_font.png" , [255]*3)
+    sfont = Font("./data/fonts/small_font.png" , [255]*3)
     
-    end_level = {
+    finished_text = Text(lfont , "Congrulations ! You found the tea recipe !")
+    finished_text.origin = finished_text.size/2
+    finished_text.pos = pygame.Vector2(200 , 75)
+    
+    click_text = Text(sfont , "click for next level ... (or maybe ?)")
+    click_text.origin = pygame.Vector2(click_text.size.x / 2 , 0)
+    click_text.pos = pygame.Vector2(200 , 290 - click_text.size.y )
+    
+    gameover_text = Text(sfont , "Game over !!!")
+    gameover_text.origin = gameover_text.size / 2
+    gameover_text.pos = pygame.Vector2(200 , 150)
+    
+    panel_datas = {
         "surface":pygame.Surface([400 , 300] , SRCALPHA),
-        "alpha":0
+        "alpha":0,
+        "panel_list":{
+            "end_level":pygame.Surface([400 , 300] , SRCALPHA),
+            "game_over":pygame.Surface([400 , 300] , SRCALPHA)
+        }
     }
     
-    end_level["surface"].fill([0,0,0])
-    finished_text.display(end_level["surface"])
+    panel_datas["panel_list"]["end_level"].fill([0,0,0])
+    panel_datas["panel_list"]["game_over"].fill([0,0,0])
+    finished_text.display(panel_datas["panel_list"]["end_level"])
+    click_text.display(panel_datas["panel_list"]["end_level"])
+    gameover_text.display(panel_datas["panel_list"]["game_over"])
     
+    tea_cup = pygame.image.load("./data/tea.png").convert_alpha()
+    panel_datas["panel_list"]["end_level"].blit(pygame.transform.scale(tea_cup , [128 , 128]) , [200-64 , 150])
+    
+    state = "menu"
+    
+    def start_game():
+        nonlocal state
+        state = "game"
+    
+    button_image = pygame.image.load("./data/start-button.png").convert_alpha()
+    button_image = pygame.transform.scale(button_image , [button_image.get_width()*4 , button_image.get_height()*4])
+    
+    start_button_data = {
+        "target":start_game,
+        "text-data":{
+            "content":"",
+            "font":pygame.font.Font(None , 30),
+            "centered":True
+        },"textures":{
+            "nothing":button_image,
+            "hover":button_image,
+            "clicked":button_image,
+        }
+    }
+    
+    button = Button([350 , 400] , [128 , 128] , start_button_data)
+    
+    menu_image = pygame.image.load("./data/menu.PNG").convert_alpha()
     
     while True:
         
-        chunk_pos = player.hitbox.rect.pos // (4*32)
-        dt = time.time() - timepoint
-        timepoint = time.time()
-        
-        camera.erase_surf([38, 27, 74])
-        black_filter.fill([0,0,0,120])
-        
-        for event in pygame.event.get():
+        if state == "game":
+            chunk_pos = player.hitbox.rect.pos // (4*32)
+            dt = time.time() - timepoint
+            timepoint = time.time()
             
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit(0)
-            elif event.type == KEYDOWN:
-                if control:
-                    if event.key == K_q:
-                        player.km["left"] = True
-                    if event.key == K_d:
-                        player.km["right"] = True
-                    if event.key == K_z:
-                        player.km["up"] = True
-                    if event.key == K_s:
-                        player.km["down"] = True
-                if event.key == K_o:
-                    for obj in objectlist.values():
-                        pos = pygame.Vector2(float(value["x"])+float(value["width"])/2 , float(value["y"])+float(value["height"])/2)
-                        if pos.distance_to(player.hitbox.rect.pos) <= 48:
-                            obj["texture"] = tilemap.tileset[39]
-                            if obj["tea recipe"]:
-                                level_ended = True
-                                control = False
-                if event.key == K_p:
-                    if power["hide"]["cooldown"] - power["hide"]["timer"] and not power["hide"]["active"]:
-                        power["hide"]["timer"] = 0
-                        power["hide"]["active"] = True
-                        player.hidden = True
-                        
-            elif event.type == KEYUP:
-                if control:
-                    if event.key == K_q:
-                        player.km["left"] = False
-                    if event.key == K_d:
-                        player.km["right"] = False
-                    if event.key == K_z:
-                        player.km["up"] = False
-                    if event.key == K_s:
-                        player.km["down"] = False
-        
-        if not level_ended:
-            power["hide"]["timer"] += dt
+            camera.erase_surf([38, 27, 74])
+            black_filter.fill([0,0,0,120])
             
-            if power["hide"]["active"] and power["hide"]["time"] - power["hide"]["timer"] <= 0:
-                player.hidden = False
-                power["hide"]["active"] = False
-            
-            colliders = []
-            
-            for y in range(-1 , 2):
-                for x in range(-1 , 2):
-                    try:
-                        colliders.extend(tilemap.collider_chunks[f"{int(chunk_pos.x+x)},{int(chunk_pos.y+y)}"])
-                    except:
-                        pass
-            
-            player.update(dt)
-            player.move(colliders)
-            
-            if not power["hide"]["active"]:
-                colliders.append(player.hitbox)
-            
-            enemy.update(dt , colliders)
-            
-            camera.pos = player.hitbox.rect.pos + player.hitbox.rect.size / 2 - camera.size / 2
+            for event in pygame.event.get():
                 
-            if camera.pos.x < 0:
-                camera.pos.x = 0
-            if camera.pos.x > tilemap.size[0]*32-camera.size.x:
-                camera.pos.x = tilemap.size[0]*32-camera.size.x
-            if camera.pos.y < 0:
-                camera.pos.y = 0
-            if camera.pos.y > tilemap.size[1]*32-camera.size.y:
-                camera.pos.y = tilemap.size[1]*32-camera.size.y
-        else:
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit(0)
+                elif event.type == KEYDOWN:
+                    if control:
+                        if event.key == K_q:
+                            player.km["left"] = True
+                        if event.key == K_d:
+                            player.km["right"] = True
+                        if event.key == K_z:
+                            player.km["up"] = True
+                        if event.key == K_s:
+                            player.km["down"] = True
+                    if event.key == K_o:
+                        for obj in objectlist.values():
+                            pos = pygame.Vector2(float(value["x"])+float(value["width"])/2 , float(value["y"])+float(value["height"])/2)
+                            if pos.distance_to(player.hitbox.rect.pos) <= 48:
+                                obj["texture"] = tilemap.tileset[39]
+                                if obj["tea recipe"]:
+                                    panel_datas["surface"] = panel_datas["panel_list"]["end_level"]
+                                    level_ended = True
+                                    control = False
+                    if event.key == K_p:
+                        if power["hide"]["cooldown"] - power["hide"]["timer"] and not power["hide"]["active"]:
+                            power["hide"]["timer"] = 0
+                            power["hide"]["active"] = True
+                            player.hidden = True
+                            
+                elif event.type == KEYUP:
+                    if control:
+                        if event.key == K_q:
+                            player.km["left"] = False
+                        if event.key == K_d:
+                            player.km["right"] = False
+                        if event.key == K_z:
+                            player.km["up"] = False
+                        if event.key == K_s:
+                            player.km["down"] = False
             
-            if end_level["alpha"] < 180:
-                end_level["alpha"] += 70 * dt
+            if not level_ended:
+                power["hide"]["timer"] += dt
+                
+                if power["hide"]["active"] and power["hide"]["time"] - power["hide"]["timer"] <= 0:
+                    player.hidden = False
+                    power["hide"]["active"] = False
+                
+                colliders = []
+                
+                for y in range(-1 , 2):
+                    for x in range(-1 , 2):
+                        try:
+                            colliders.extend(tilemap.collider_chunks[f"{int(chunk_pos.x+x)},{int(chunk_pos.y+y)}"])
+                        except:
+                            pass
+                
+                player.update(dt)
+                player.move(colliders)
+                
+                if not power["hide"]["active"]:
+                    colliders.append(player.hitbox)
+                
+                enemy.update(dt , colliders)
+                
+                camera.pos = player.hitbox.rect.pos + player.hitbox.rect.size / 2 - camera.size / 2
+                    
+                if camera.pos.x < 0:
+                    camera.pos.x = 0
+                if camera.pos.x > tilemap.size[0]*32-camera.size.x:
+                    camera.pos.x = tilemap.size[0]*32-camera.size.x
+                if camera.pos.y < 0:
+                    camera.pos.y = 0
+                if camera.pos.y > tilemap.size[1]*32-camera.size.y:
+                    camera.pos.y = tilemap.size[1]*32-camera.size.y
+            else:
+                
+                if panel_datas["alpha"] < 220:
+                    panel_datas["alpha"] += 70 * dt
 
-        
-        tilemap.display(camera.render_surf , camera.pos)
-        
-        y = 0
-        player_displayed = False
-        for value in objectlist.values():
-            y = float(value["y"])
-            if not player_displayed and y > player.hitbox.rect.bottom:
+            
+            tilemap.display(camera.render_surf , camera.pos)
+            
+            y = 0
+            player_displayed = False
+            for value in objectlist.values():
+                y = float(value["y"])
+                if not player_displayed and y > player.hitbox.rect.bottom:
+                    player.display(camera.render_surf , camera.pos)
+                    player_displayed = True
+                camera.render_surf.blit(value["texture"] , pygame.Vector2(float(value["x"]) , float(value["y"])-float(value["height"])) - camera.pos)
+            
+            
+            if not player_displayed:
                 player.display(camera.render_surf , camera.pos)
                 player_displayed = True
-            camera.render_surf.blit(value["texture"] , pygame.Vector2(float(value["x"]) , float(value["y"])-float(value["height"])) - camera.pos)
-        
-        
-        if not player_displayed:
-            player.display(camera.render_surf , camera.pos)
-            player_displayed = True
-        
-        enemy.display(camera.render_surf , camera.pos)
-        
-        enemy.display_light(black_filter , camera.pos)
-        
-        for collider in colliders:
-            collider.rect.draw(camera.render_surf , camera.pos , True)
-           
-        camera.render_surf.blit(black_filter , [0,0])
             
-        if level_ended:
-            surf = end_level['surface'].copy()
-            surf.set_alpha(0+end_level["alpha"])
-            camera.render_surf.blit(surf , [0,0])
+            enemy.display(camera.render_surf , camera.pos)
+            
+            enemy.display_light(black_filter , camera.pos)
+                
+            if level_ended:
+                surf = panel_datas['surface'].copy()
+                surf.set_alpha(0+panel_datas["alpha"])
+                camera.render_surf.blit(surf , [0,0])
+            else:
+                camera.render_surf.blit(black_filter , [0,0])
+            
+            camera.display(screen , screen.get_rect())
         
-        
-        camera.display(screen , screen.get_rect())
+        else:
+            
+            screen.fill([0,0,0])
+            
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit(0)
+                
+                button.update(event)
+            
+            screen.blit(menu_image , [400-583/2 , 10])
+            
+            button.display(screen)
+            
         
         pygame.display.flip()
 
