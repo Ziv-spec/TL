8a1eb99d2610ce004923a2be7741570a25a8c3b6
Lody <69472620+bilhox@users.noreply.github.com>
2022-08-08 13:59:27 +0200

    V0.6

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pygame.locals import *
 from scripts.camera import *
 from scripts.entity import *
 from scripts.map import TileMap
+from scripts.text import *
 
 
 def main():
@@ -46,6 +47,14 @@ def main():
     enemy = Enemy(Collider(FloatRect(pygame.Vector2(416+128 , 1280-128) , pygame.Vector2(16 , 16)) , "block"))
     enemy.set_direction(pygame.Vector2(0 , -1))
     
+    control = True
+    
+    power = {"hide":{"cooldown":2 , "timer":0 , "time":5 , "active":False}}
+    
+    level_ended = False
+    
+    font = Font("./data/fonts/large_font.png")
+    
     while True:
         
         chunk_pos = player.hitbox.rect.pos // (4*32)
@@ -61,56 +70,76 @@ def main():
                 pygame.quit()
                 sys.exit(0)
             elif event.type == KEYDOWN:
-                if event.key == K_q:
-                    player.km["left"] = True
-                if event.key == K_d:
-                    player.km["right"] = True
-                if event.key == K_z:
-                    player.km["up"] = True
-                if event.key == K_s:
-                    player.km["down"] = True
+                if control:
+                    if event.key == K_q:
+                        player.km["left"] = True
+                    if event.key == K_d:
+                        player.km["right"] = True
+                    if event.key == K_z:
+                        player.km["up"] = True
+                    if event.key == K_s:
+                        player.km["down"] = True
                 if event.key == K_o:
                     for obj in objectlist.values():
                         pos = pygame.Vector2(float(value["x"])+float(value["width"])/2 , float(value["y"])+float(value["height"])/2)
                         if pos.distance_to(player.hitbox.rect.pos) <= 48:
                             obj["texture"] = tilemap.tileset[39]
+                            if obj["tea recipe"]:
+                                level_ended = True
+                                control = False
+                if event.key == K_p:
+                    if power["hide"]["cooldown"] - power["hide"]["timer"] and not power["hide"]["active"]:
+                        power["hide"]["timer"] = 0
+                        power["hide"]["active"] = True
+                        player.hidden = True
+                        
             elif event.type == KEYUP:
-                if event.key == K_q:
-                    player.km["left"] = False
-                if event.key == K_d:
-                    player.km["right"] = False
-                if event.key == K_z:
-                    player.km["up"] = False
-                if event.key == K_s:
-                    player.km["down"] = False
+                if control:
+                    if event.key == K_q:
+                        player.km["left"] = False
+                    if event.key == K_d:
+                        player.km["right"] = False
+                    if event.key == K_z:
+                        player.km["up"] = False
+                    if event.key == K_s:
+                        player.km["down"] = False
         
-        colliders = []
-        
-        for y in range(-1 , 2):
-            for x in range(-1 , 2):
-                try:
-                    colliders.extend(tilemap.collider_chunks[f"{int(chunk_pos.x+x)},{int(chunk_pos.y+y)}"])
-                except:
-                        pass
-        
-        player.update(dt)
-        player.move(colliders)
-        
-        colliders.append(player.hitbox)
-        
-        enemy.update(dt , colliders)
-        
-        camera.pos = player.hitbox.rect.pos + player.hitbox.rect.size / 2 - camera.size / 2
+        if not level_ended:
+            power["hide"]["timer"] += dt
             
-        if camera.pos.x < 0:
-            camera.pos.x = 0
-        if camera.pos.x > tilemap.size[0]*32-camera.size.x:
-            camera.pos.x = tilemap.size[0]*32-camera.size.x
-        if camera.pos.y < 0:
-            camera.pos.y = 0
-        if camera.pos.y > tilemap.size[1]*32-camera.size.y:
-            camera.pos.y = tilemap.size[1]*32-camera.size.y
+            if power["hide"]["active"] and power["hide"]["time"] - power["hide"]["timer"] <= 0:
+                player.hidden = False
+                power["hide"]["active"] = False
+            
+            colliders = []
+            
+            for y in range(-1 , 2):
+                for x in range(-1 , 2):
+                    try:
+                        colliders.extend(tilemap.collider_chunks[f"{int(chunk_pos.x+x)},{int(chunk_pos.y+y)}"])
+                    except:
+                        pass
+            
+            player.update(dt)
+            player.move(colliders)
+            
+            if not power["hide"]["active"]:
+                colliders.append(player.hitbox)
+            
+            enemy.update(dt , colliders)
+            
+            camera.pos = player.hitbox.rect.pos + player.hitbox.rect.size / 2 - camera.size / 2
                 
+            if camera.pos.x < 0:
+                camera.pos.x = 0
+            if camera.pos.x > tilemap.size[0]*32-camera.size.x:
+                camera.pos.x = tilemap.size[0]*32-camera.size.x
+            if camera.pos.y < 0:
+                camera.pos.y = 0
+            if camera.pos.y > tilemap.size[1]*32-camera.size.y:
+                camera.pos.y = tilemap.size[1]*32-camera.size.y
+        else:
+            ...       
         
         tilemap.display(camera.render_surf , camera.pos)
         
